25c2ac0dc99ded06376fd3e3e2e43117dcfd9d54
albertkyou <albert.you@berkeley.edu>
2020-04-03 17:05:40 -0700

    Update sudokusolver.py

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,28 +1,34 @@
 # take in board as a list
 import numpy as np 
 
+
 def mini(board):
+    # main loop to fill in the board
+    for i in range(10):
+        board = fill_hole(board)
+    
+    print_board(board)
+
+def fill_hole(board):
     # solves 4x4 sudoku boards
     # brute force recursion
     # start at top left cell and insert lowest possible value that does not interfere with the rest of the board
     # once a successful value is found, move onto the next cell
     # if no cells are possible, remove previous cell and go to the next lowest possible value
 
-    bnp = np.array(board)
-
     # find first 0 and replace with x
     for row in range(4):
         for col in range(4):
-            if bnp[row,col]==0:
-                bnp[row,col]=1
-                print_board(bnp)
-                
-                if test_legality(bnp):
-                    return mini(bnp)
-                else:
-                    return False
+            if board[row,col]==0:
+                print_board(board)
+                board[row,col]=1 # set it as 1
+                if test_legality(board):
+                    return board # if 1 works, then move on to the next cell
 
-    return
+                else:
+                    board[row,col]+=1
+                    return board
+
 
 def test_legality(board):
     # test if the current board is legal
@@ -58,13 +64,16 @@ def test_legality(board):
 
 
 def print_board(board):
+    print('board: ')
     for row in range(4):
+        
         print(board[row]) # 0 indicates empty cell
 
 # Test case for 4x4 puzzle
 def main():
     board = [[4,3,0,0],[1,2,3,0],[0,0,2,0],[2,1,0,0]]
-    mini(board)
+    bnp = np.array(board)
+    mini(bnp)
 
 
 if __name__ == "__main__":
