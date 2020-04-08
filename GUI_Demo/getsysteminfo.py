cc2e5e57653f63f562f27d093f89adf2bd4bd5f7
albertkyou <albert.you@berkeley.edu>
2020-04-07 23:54:23 -0700

    Create getsysteminfo.py

---
+++ GUI_Demo/getsysteminfo.py
@@ -0,0 +1,11 @@
+import tkinter as tk 
+from tkinter import Text
+
+window = tk.Tk()
+window.title('System Info')
+
+
+greeting = tk.Label(text='Welcome',height=10)
+greeting.pack()
+
+window.mainloop()
